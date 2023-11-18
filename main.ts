ad45b83ee17d163ed91e804a3e999b45215e7e63
toridoriv <vrodriguezfe@icloud.com>
2023-11-17 21:39:58 -0300

    :monocle_face: Make change to trigger action

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
 Deno.serve(() =>
-  new Response("🎶🎶 Que llegaras con rosas, con mil rosas para mí 🎶🎶")
+  new Response(
+    "🎶🎶 Que llegaras con rosas, con mil rosas para mí, porque ya sabes que me encantan esas cosas soy así 🎶🎶",
+  )
 );
