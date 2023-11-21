09baf9cf89835b71889936ecf56d64a376fc0f6d
toridoriv <vrodriguezfe@icloud.com>
2023-11-21 19:40:05 -0300

    :recycle: Use backticks

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 Deno.serve(() =>
   new Response(
-    "<h1>Sweet but Psycho</h1><p>🎶 Oh, she's sweet but a psycho 🎶</p>",
+    `<h1>Sweet but Psycho</h1>
+    <p>🎶 Oh, she's sweet but a psycho 🎶</p>`,
     { headers: { "content-type": "text/html" } },
   )
 );
