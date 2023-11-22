66a96a72abc5f918664481137ce8409644140c2a
toridoriv <vrodriguezfe@icloud.com>
2023-11-22 19:26:52 -0300

    :speech_balloon: Add line

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 Deno.serve(() =>
   new Response(
     `<h1>Sweet but Psycho</h1>
-    <p>🎶 Oh, she's sweet but a psycho 🎶</p>`,
+    <p>🎶 Oh, she's sweet but a psycho 🎶</p>
+    <p>🎶 Oh, she's sweet but a psycho 🎶</p>
+    `,
     { headers: { "content-type": "text/html" } },
   )
 );
